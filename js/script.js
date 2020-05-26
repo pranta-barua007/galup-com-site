cf42c6ad75b890717866cdef0cbfa9e07860f79b
pranta-barua007 <prantabarua247@gmail.com>
2020-05-26 21:05:40 +0600

    added new scroll system

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+			$(document).ready(function (){
+				$("#test").click(function (){
+					$("#test1").slideToggle();
+				});
+				$("#test2").click(function (){
+					$("#test3").slideToggle();
+				});
+				$("#test4").click(function (){
+					$("#test5").slideToggle();
+				});
+			});
